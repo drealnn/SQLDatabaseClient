de8c5ccf65d602179e5c246ebd605ccf8f9eb88b
Daniel <drealnn@gmail.com>
2016-03-01 12:11:38 -0500

    init commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,151 @@
+package sample;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.embed.swing.SwingNode;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import javax.swing.*;
+import java.net.URL;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public TextArea queryText;
+    public Button submitBut;
+    public SwingNode swingNode;
+
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DATABASE_URL = "jdbc:mysql://localhost:3306/bikedb";
+    static final String USERNAME= "root";
+    static final String PASSWORD= "root";
+    static final String DEFAULT_QUERY = "SELECT * FROM bikes\nwhere color = \"blue\"";
+    public ChoiceBox driverBox;
+    public ChoiceBox databaseBox;
+    public TextField usernameBox;
+    public TextField passwordBox;
+    public Button ClearBt;
+    public Button connectBt;
+    public Label connectionLabel;
+
+    String database;
+    String username;
+    String password;
+    String driver;
+    String query;
+
+    private ResultSetTableModel tableModel;
+
+    public void submitQuery(ActionEvent actionEvent) {
+        try
+        {
+            // create TableModel for results of query SELECT * FROM bikes
+            username = usernameBox.getText();
+            password = passwordBox.getText();
+            query = queryText.getText();
+            tableModel = new ResultSetTableModel( driver, database,username, password, query );
+
+            // create JTable delegate for tableModel
+            JTable resultTable = new JTable( tableModel );
+
+            JScrollPane scroll = new JScrollPane(resultTable);
+
+            swingNode.setContent(scroll);
+
+            /*
+            // perform a new query
+            try
+            {
+                tableModel.setQuery( queryArea.getText() );
+            } // end try
+            catch ( SQLException sqlException )
+            {
+                JOptionPane.showMessageDialog( null,
+                        sqlException.getMessage(), "Database error",
+                        JOptionPane.ERROR_MESSAGE );
+
+                // try to recover from invalid user query
+                // by executing default query
+                try
+                {
+                    tableModel.setQuery( DEFAULT_QUERY );
+                    queryArea.setText( DEFAULT_QUERY );
+                } // end try
+                catch ( SQLException sqlException2 )
+                {
+                    JOptionPane.showMessageDialog( null,
+                            sqlException2.getMessage(), "Database error",
+                            JOptionPane.ERROR_MESSAGE );
+
+                    // ensure database connection is closed
+                    tableModel.disconnectFromDatabase();
+
+                    System.exit( 1 ); // terminate application
+                } // end inner catch
+            } // end outer catch
+            */
+
+        } // end try
+        catch ( ClassNotFoundException classNotFound )
+        {
+            JOptionPane.showMessageDialog( null,
+                    "MySQL driver not found", "Driver not found",
+                    JOptionPane.ERROR_MESSAGE );
+
+            System.exit( 1 ); // terminate application
+        } // end catch
+        catch ( SQLException sqlException )
+        {
+            JOptionPane.showMessageDialog( null, sqlException.getMessage(),
+                    "Database error", JOptionPane.ERROR_MESSAGE );
+
+            // ensure database connection is closed
+            tableModel.disconnectFromDatabase();
+
+            System.exit( 1 );   // terminate application
+        } // end catch
+    }
+
+    public void connectToDatabase(ActionEvent actionEvent) {
+        /*Class.forName( driver );
+
+        // connect to database
+        connection = DriverManager.getConnection(url, username, password);
+
+        // create Statement to query database
+        statement = connection.createStatement(
+                ResultSet.TYPE_SCROLL_INSENSITIVE,
+                ResultSet.CONCUR_READ_ONLY );
+
+        // update database connection status
+        connectedToDatabase = true;*/
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        databaseBox.setItems(FXCollections.observableArrayList(DATABASE_URL));
+
+        databaseBox.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                database = (String) newValue;
+
+            }
+        });
+
+        driverBox.setItems(FXCollections.observableArrayList(JDBC_DRIVER));
+        driverBox.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                driver = (String) newValue;
+
+            }
+        });
+
+    }
+}
